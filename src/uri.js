f63088c6ec374cc21d6fd2c7dc6c294a5c57fbce
Bidesh <bideshbanerjee444@gmail.com>
2022-11-28 18:59:34 +0530

    heroku service removed

--- src/uri.js
+++ src/uri.js
@@ -1,5 +1,5 @@
-import {} from 'dotenv/config';
-export const URL =  "https://nutty-stockings-wasp.cyclic.app";
+
+export const URL =  "https://nightchillinsbackend-production.up.railway.app";
 export const uploadURL = "https://api.cloudinary.com/v1_1/dnbpwbzkv/image/upload";
 export const uploadVideoURL =  "https://api.cloudinary.com/v1_1/dnbpwbzkv/video/upload";
 export const preset =  "jmixsgj4";
