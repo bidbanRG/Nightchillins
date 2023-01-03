4451eba627ea0c5b23aa5466640382c49bc5f660
Bidesh <bideshbanerjee444@gmail.com>
2023-01-03 13:17:58 +0530

    api-key-updated

--- src/uri.js
+++ src/uri.js
@@ -1,5 +1,5 @@
 
-export const URL =  "https://nightchillinsbackend-production.up.railway.app";
+export const URL =  "https://nightchillins-api-kcby.vercel.app";
 export const uploadURL = "https://api.cloudinary.com/v1_1/dnbpwbzkv/image/upload";
 export const uploadVideoURL =  "https://api.cloudinary.com/v1_1/dnbpwbzkv/video/upload";
 export const preset =  "jmixsgj4";
